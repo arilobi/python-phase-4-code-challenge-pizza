da3f28b791f1d2334fce85349165114c7ded2f79
Marion Okondo <arinabulobi@gmail.com>
2025-01-26 16:54:59 +0300

    add pizza challenge

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -24,6 +24,104 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+# ---> Getting the restaurants
+@app.route("/restaurants", methods=["GET"])
+def fetch_restaurants():
+    restaurants = Restaurant.query.all()
+
+    restaurant_list = []
+
+    for restaurant in restaurants:
+        restaurant_list.append(restaurant.to_dict(only=(
+            'id', 
+            'name', 
+            'address'
+        )))
+    return jsonify(restaurant_list), 200
+
+# ---> Getting the restaurant by id
+@app.route("/restaurants/<int:id>", methods=["GET"])
+def single_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+
+    if restaurant:
+        return jsonify(restaurant.to_dict(only=(
+            'id', 
+            'name', 
+            'address', 
+            'restaurant_pizzas.pizza', 
+            'restaurant_pizzas.id', 
+            'restaurant_pizzas.price', 
+            'restaurant_pizzas.pizza_id', 
+            'restaurant_pizzas.restaurant_id'
+        ))), 200
+    
+    else:
+        return jsonify({"error": "Restaurant not found"}), 404
+    
+# ---> Delete restaurants by id
+@app.route("/restaurants/<int:id>", methods=["DELETE"])
+def delete_restaurant(id): 
+    restaurant = Restaurant.query.get(id)
+
+    if restaurant: 
+
+        RestaurantPizza.query.filter_by(restaurant_id=id).delete()
+        
+        db.session.delete(restaurant)
+        db.session.commit()
+        return '', 204 # ---> means no content but the request is successful
+    else:
+        return jsonify({"error": "Restaurant not found"})
+
+# ---> Fetching pizzas
+@app.route("/pizzas", methods=["GET"])
+def fetch_pizzas():
+    pizzas = Pizza.query.all()
+
+    pizza_list = []
+
+    for pizza in pizzas:
+        pizza_list.append(pizza.to_dict(only=(
+            'id', 
+            'name', 
+            'ingredients'
+        )))
+    return jsonify(pizza_list), 200
+
+# ---> Add a new restaurant_pizza
+@app.route("/restaurant_pizzas", methods=["POST"])
+def create_restaurant_pizza():
+    data = request.get_json()
+
+    # Checking id the restaurant and pizza exists
+    restaurant = Restaurant.query.get(data.get('restaurant_id'))
+    pizza = Pizza.query.get(data.get('pizza_id'))
+
+    if not restaurant or not pizza:
+        return jsonify({"errors": ["Restaurant and Pizza doesn't exist"]}), 406
+    
+    # creating the new RestaurantPizza with validation
+    try:
+        new_restaurant_pizza = RestaurantPizza(
+            price = data.get('price'),
+            pizza_id = data.get('pizza_id'),
+            restaurant_id = data.get('restaurant_id'),
+        )
+
+        db.session.add(new_restaurant_pizza)
+        db.session.commit()
+
+        response_data = new_restaurant_pizza.to_dict(only=(
+            'id', 
+            'price', 
+            'pizza', 
+            'restaurant'
+        ))
+        return jsonify(response_data), 201
+    
+    except ValueError:
+        return jsonify({"errors": ["validation errors"]}), 400
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -20,9 +20,12 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # add relationship
+    # add relationship and cascade delete orphan
+    restaurant_pizzas = relationship("RestaurantPizza", back_populates="restaurant", cascade='all, delete-orphan')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -35,9 +38,12 @@ class Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-    # add relationship
+    # add relationship and cascade delete orphan
+    restaurant_pizzas = relationship("RestaurantPizza", back_populates="pizza", cascade='all, delete-orphan')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -49,11 +55,22 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
+    # Add foreign keys
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+
     # add relationships
+    restaurant = relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = relationship("Pizza", back_populates="restaurant_pizzas")
 
     # add serialization rules
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
 
     # add validation
-
+    @validates('price')
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30): 
+            raise ValueError("Price must be between 1 and 30.")
+        return value
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
